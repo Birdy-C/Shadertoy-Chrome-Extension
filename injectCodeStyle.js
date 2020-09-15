96c67cee980c134a60c0e41b86e7283a41082732
Birdy-C <xiaoyuan2425@126.com>
2020-09-15 20:55:08 +0800

    change icon and code style selection

--- injectCodeStyle.js
+++ injectCodeStyle.js
@@ -1,4 +1,4 @@
-const kCodeStyle = ['3024-day', '3024-night', 'abcdef', 'ambiance', 'ayu-dark', 'ayu-mirage', 'base16-dark', 'base16-light', 'bespin', 'blackboard', 'cobalt', 'colorforth', 'darcula', 'dracula', 'duotone-dark', 'duotone-light', 'eclipse', 'elegant', 'erlang-dark', 'gruvbox-dark', 'hopscotch', 'icecoder', 'idea', 'isotope', 'lesser-dark', 'liquibyte', 'lucario', 'material', 'material-darker', 'material-palenight', 'material-ocean', 'mbo', 'mdn-like', 'midnight', 'monokai', 'moxer', 'neat', 'neo', 'night', 'nord', 'oceanic-next', 'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'rubyblue', 'seti', 'shadowfox', 'solarized', 'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'ttcn', 'twilight', 'vibrant-ink', 'xq-dark', 'xq-light', 'yeti', 'yonce', 'zenburn'];
+const kCodeStyle = ['ambiance', 'darcula', 'eclipse', 'elegant', 'lesser-dark', 'material-palenight', 'neat', 'neo', 'nord', 'oceanic-next', 'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'shadowfox', 'twilight', 'vibrant-ink', 'xq-light'];
 var storedCodeStyle = 0
 
 function constructLink(kCodeStyle) {
